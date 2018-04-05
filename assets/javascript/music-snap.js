aebae8786021209604db682a1547088e9a60f8d3
Edward Finley <biged621@gmail.com>
2018-04-05 19:54:48 -0400

    changed a thing back

--- assets/javascript/music-snap.js
+++ assets/javascript/music-snap.js
@@ -96,54 +96,41 @@ var template = function (data) {
         });
     };
     return `
-    <div class="main-wrapper">
-      <div class="now-playing__img">
-        <img src="${data.item.album.images[0].url}">
-      </div>
-      <div class="now-playing__side">
-        <div class="now-playing__name">${data.item.name}</div>
-        <div class="now-playing__artist">${data.item.artists[0].name}</div>
-        <div class="now-playing__status">${data.is_playing ? 'Playing' : 'Paused'}</div>
-        <div class="progress">
-          <div class="progress__bar" style="width:${data.progress_ms * 100 / data.item.duration_ms}%"></div>
-        </div>
-        <p>
-  <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
-  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
-  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>
-</p>
-<div class="row">
-  <div class="col">
-    <div class="collapse multi-collapse" id="multiCollapseExample1">
-      <div class="card card-body">
-      <div class="lyrics"></div>
-      <div id="artist-bio">
-      </div>
-      </div>
+<div class="main-wrapper">
+    <div class="now-playing__img">
+    <img src="${data.item.album.images[0].url}">
     </div>
-  </div>
-  <div class="col">
-    <div class="collapse multi-collapse" id="multiCollapseExample2">
-      <div class="card card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-      </div>
+    <div class="now-playing__side">
+    <div class="now-playing__name">${data.item.name}</div>
+    <div class="now-playing__artist">${data.item.artists[0].name}</div>
+    <div class="now-playing__status">${data.is_playing ? 'Playing' : 'Paused'}</div>
+    <div class="progress">
+        <div class="progress__bar" style="width:${data.progress_ms * 100 / data.item.duration_ms}%"></div>
     </div>
-  </div>
-</div>
-    
-        <div id="relatedartist">
-            <table class="table table-hover" id='related-artist-table'>
+    <div class="lyrics"></div>
+    <div id="artist-bio">
+    </div>
+        <div id="artist">
+            <table class="table table-hover" id='artist-table'>
                     <thead>
                     </thead>
                     <tbody>
                     </tbody>
-                    </table>
+            </table>
     
     </div>
-      </div>
+    <div id="relatedartist">
+        <table class="table table-hover" id='related-artist-table'>
+                <thead>
+                </thead>
+                <tbody>
+                </tbody>
+        </table>
     </div>
-    <div class="background" style="background-image:url(${data.item.album.images[0].url})"></div>
-  `;
+</div>
+</div>
+<div class="background" style="background-image:url(${data.item.album.images[0].url})"></div>
+`;
 
 }
 
